69885f11aa6b00c829aecae46f5d86d3b7b7fcb7
Everton Fernandes <evertonfernandes.jsx@gmail.com>
2024-04-15 21:31:59 -0300

    feat: :sparkles: config authentication and generating jwt token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { PrismaService } from './prisma/prisma.service'
+import { envSchema } from './env'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { authModule } from './auth/auth.module'
 
 @Module({
-  imports: [],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    authModule,
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
